572814755f4de5bf053ea36834c6feaae269c029
Faradenzas <farhad020202@gmail.com>
2021-08-15 14:03:05 +0500

    edited section scrolling

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-window.scroll(0, 0);
-
-
-let width = visualViewport.width;
-let height = visualViewport.height;
-console.log(width)
-console.log(height)
-
-let sections = document.querySelectorAll('section');
-
-for (let i = 0; i < sections.length; i++) {
-    sections[i].addEventListener('click', (e) => {
-        let step = (e.screenX > width / 2) ? 1 : -1;
-        let x = width * (i + step);
-        let y = height * (i + step);
-        window.scroll(x, y);
-        console.log(step, x, y)
-    });
-    sections[i].innerHTML += "<div class=\"arrow-wrapper\"> <div class=\"right arrow\"></div> </div> <div class=\"arrow-wrapper\"> <div class=\"left arrow\"></div> </div>";
-}
-
 
 
 
